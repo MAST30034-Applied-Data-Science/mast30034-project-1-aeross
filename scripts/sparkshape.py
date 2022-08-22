f23ba4fe25ce2c42f2327c6f0ecc9bf0f7b7c032
Andrew <andrewpratama22@gmail.com>
2022-08-22 14:23:11 +1000

    removed sparkshape

--- scripts/sparkshape.py
+++
@@ -1,9 +0,0 @@
-def spark_shape(sdf):
-    """
-    This function sort of imitates pandas' df.shape().
-    It takes a spark dataframe as input and returns the number of rows
-    and columns of the dataframe in this tuple format: (rows, columns).
-    """
-    output = (sdf.count(), len(sdf.columns))
-
-    return output
